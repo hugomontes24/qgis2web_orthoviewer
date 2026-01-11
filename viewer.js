513d75b85cfb404d5057722518b25d13d81961a5
Hugo Machado <hugomontes@hotmail.fr>
2026-01-11 22:20:40 +0100

    fixing lil bugs

--- viewer.js
+++ viewer.js
@@ -4,6 +4,14 @@ const spanLng = document.getElementById("lng");
 const spanLat = document.getElementById("lat");
 const spanPhotoName = document.getElementById("photo_name");
 
+const btn2D = document.getElementById("btn-2d");
+const btn3D = document.getElementById("btn-3d");
+
+const btnFullscreen = document.getElementById("btn_fullscreen");
+const mapDiv = document.getElementById("map_div");
+const sidebarDiv =  document.getElementById("sidebar_div");
+
+
 
 // **************************
 // ****  MAIN EXECUTION  *****
@@ -16,6 +24,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
             chosenPhoto = getPhotoById(e.data.id); // class Photo
             const [lng,lat] = chosenPhoto.coordinates
+
+            console.log(lng,lat);
+            
             
             chosenPictureElmt.src = `${basepath}${chosenPhoto.name}`;
             spanPhotoName.innerHTML= chosenPhoto.name;
@@ -27,6 +38,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
     }); 
+
+    ['click','mousedown','wheel','touchstart'].forEach(evt => {
+        btnFullscreen.addEventListener(evt, e => e.stopPropagation());
+    });
+
+    btnFullscreen.addEventListener("click", () => {
+        toggleActiveBtn(btnFullscreen);
+        mapDiv.classList.toggle("fullscreen_mode");
+        sidebarDiv.classList.toggle("fullscreen_mode");
+    });
+
+
+    
 
 
 
